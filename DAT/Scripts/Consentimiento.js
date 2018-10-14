31cb86e4b706a7c3849df18153767479286099a6
Guidoher18 <guido_181193@outlook.com>
2018-10-14 04:04:57 -0300

    Otros cambios visuales

--- DAT/Scripts/Consentimiento.js
+++ DAT/Scripts/Consentimiento.js
@@ -1,7 +1,1 @@
-﻿/*$(document).ready(function () {
-    //Centrar Verticalmente el body>div 
-    var $Body_alto = $(window).height();
-    var $Div_alto = $('body>div').height();
-    var $Margen_total = ($Body_alto - $Div_alto)/2;
-    $('body>div').css('margin-top', $Margen_total, 'margin-bottom', $Margen_total);
-});*/
+﻿
