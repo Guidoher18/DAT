87f93c4eacf0a6d47bebcb53e90a513c080b55b2
unknown <guido_181193@outlook.com>
2020-09-26 01:32:05 -0300

    Quita la nueva publicidad

--- DAT/Scripts/PrevenirCierre.js
+++ DAT/Scripts/PrevenirCierre.js
@@ -67,9 +67,24 @@ $(document).ready(function () {
     $('center').remove();
     $('script[src="http://ads.mgmt.somee.com/serveimages/ad2/WholeInsert4.js"]').remove();
     $('body>div:not(div:first-child)').remove();
-    $('div[style="height: 65px;"]').remove();
-    $('div[style="opacity: 0.9; z-index: 2147483647; position: fixed; left: 0px; bottom: 0px; height: 65px; right: 0px; display: block; width: 100%; background-color: #202020; margin: 0px; padding: 0px;"]').remove();
-    $('div[style*="position: fixed; z-index: 2147483647; left: 0px; bottom: 0px; height: 65px; right: 0px; display: block; width: 100%; background-color: transparent; margin: 0px; padding: 0px;"]').remove();
+
+    function chauads() { 
+        $("script[lang='JavaScript']").remove();
+        $("script[src*='ads.mgmt']").remove();
+        $('div[style="height: 65px;"]').remove();
+        $('div[style="opacity: 0.9; z-index: 2147483647; position: fixed; left: 0px; bottom: 0px; height: 65px; right: 0px; display: block; width: 100%; background-color: #202020; margin: 0px; padding: 0px;"]').remove();
+        $('div[style*="position: fixed; z-index: 2147483647; left: 0px; bottom: 0px; height: 65px; right: 0px; display: block; width: 100%; background-color: transparent; margin: 0px; padding: 0px;"]').remove();
+    }
+
+    chauads();
+
+    if ($("script[lang='JavaScript']").length !== 0 || $("script[src*='ads.mgmt']").length !== 0) {
+        chauads();
+    }
+
+    setTimeout(function () {
+        chauads();
+    }, 10000);
     //---
     wireUpEvents();
 });
